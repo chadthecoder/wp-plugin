917f6ae01568154d6f0b369f4c1ccd45bd8765f8
Chad <mcadamschad@outlook.com>
2019-05-04 07:43:24 -0700

    fixed minor problems and semantics with questions

--- main.cpp
+++ main.cpp
@@ -1,37 +1,42 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+
 #include <unistd.h>
 #include <direct.h>
+#include <bits/stdc++.h>
 
 int main()
 {
-    std::string name = name, description, version, author, authorURI;
-
+    std::string name, description, version, author, authorURI;
     std::cout << "What should be the name of the plugin?" << std::endl;
-    std::cin >> name;
+    getline(std::cin, name);
     std::cout << "Give a short description of the plugin." << std::endl;
-    std::cin >> description;
+    getline(std::cin, description);
     std::cout << "What version is your plugin in?" << std::endl;
-    std::cin >> version;
+    getline(std::cin, version);
     std::cout << "Who is the author of this plugin?" << std::endl;
-    std::cin >> author;
+    getline(std::cin, author);
     std::cout << "What is the website for the plugin/author?" << std::endl;
-    std::cin >> authorURI;
-    
+    getline(std::cin, authorURI);
+
+    std::string textDomain = name;
+    transform(name.begin(), name.end(), textDomain.begin(), ::tolower);
+
     std::ofstream plugin;
     chdir("../");
     _mkdir(name.c_str());
     chdir(name.c_str());
     plugin.open("index.php");
+
     plugin  << "<?php" << std::endl
             << "/*" << std::endl
-            << " * Plugin Name: " << name.c_str() << std::endl
-            << " * Description: " << description.c_str() << std::endl
-            << " * Version: " << version.c_str() << std::endl
-            << " * Author: " << author.c_str() << std::endl
-            << " * Author URI: " << authorURI.c_str() << std::endl
-            << " * Text Domain: " << name.c_str() << std::endl
+            << " * Plugin Name: " << name << std::endl
+            << " * Description: " << description << std::endl
+            << " * Version: " << version << std::endl
+            << " * Author: " << author << std::endl
+            << " * Author URI: " << authorURI << std::endl
+            << " * Text Domain: " << textDomain << std::endl
             << " */" << std::endl << "?>";
     plugin.close();
     return 0;
